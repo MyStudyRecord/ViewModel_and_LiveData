7764f715b0a4ff64f4ab141bbad34161a11da169
ParkSangSun1 <appleasz1028102@gmail.com>
2021-03-08 23:37:14 +0900

    ViewModel을 왜 써야하는지

---
+++ app/src/main/java/com/example/viewmodels/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.viewmodels
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.viewmodels.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
+    private var count = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        binding.countText.text = count.toString()
+        binding.button.setOnClickListener {
+            count++
+            binding.countText.text = count.toString()
+
+        }
+    }
+}
